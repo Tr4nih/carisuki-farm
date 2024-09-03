922397e72f6b880860708273d488edcce4c4531b
ditsekolah <ditsekolah@gmail.com>
2024-09-03 20:53:43 +0800

    tambahan js bagian lokasi

--- move.js
+++ move.js
@@ -3,52 +3,37 @@ document.addEventListener("DOMContentLoaded", function () {
     let isDown = false;
     let startX;
     let scrollLeft;
-    let velocity = 0;
-    let momentumID;
 
-    slider.addEventListener('mousedown', (e) => {
+    slider.addEventListener('pointerdown', (e) => {
         isDown = true;
         slider.classList.add('active');
-        startX = e.pageX - slider.offsetLeft;
+        startX = e.clientX;
         scrollLeft = slider.scrollLeft;
-        cancelMomentumTracking();
     });
 
-    slider.addEventListener('mouseleave', () => {
-        if (isDown) applyMomentum();
-        isDown = false;
-        slider.classList.remove('active');
+    slider.addEventListener('pointerleave', () => {
+        if (isDown) {
+            isDown = false;
+            slider.classList.remove('active');
+        }
     });
 
-    slider.addEventListener('mouseup', () => {
-        if (isDown) applyMomentum();
-        isDown = false;
-        slider.classList.remove('active');
+    slider.addEventListener('pointerup', () => {
+        if (isDown) {
+            isDown = false;
+            slider.classList.remove('active');
+        }
     });
 
-    slider.addEventListener('mousemove', (e) => {
+    slider.addEventListener('pointermove', (e) => {
         if (!isDown) return;
         e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const run = (x - startX) * 100; // Mengurangi kecepatan drag agar lebih halus
-        velocity = run; // Set kecepatan sesuai dengan gerakan mouse
-        slider.scrollLeft = scrollLeft - run;
+        const x = e.clientX;
+        const speed = x - startX;
+        slider.scrollLeft = scrollLeft - speed * 3; // Update scroll position directly
     });
-
-    function applyMomentum() {
-        cancelMomentumTracking();
-        momentumID = requestAnimationFrame(momentumLoop);
-    }
-
-    function cancelMomentumTracking() {
-        cancelAnimationFrame(momentumID);
-    }
-
-    function momentumLoop() {
-        slider.scrollLeft -= velocity;
-        velocity *= 0; // Mengurangi kecepatan secara bertahap
-        if (Math.abs(velocity) > 0) {
-            momentumID = requestAnimationFrame(momentumLoop);
-        }
-    }
 });
+
+
+
+
